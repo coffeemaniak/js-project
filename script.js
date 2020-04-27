66a0fd5527fb7279196456ae3b6cd3a224464c64
coffeemaniak <derrykain@gmail.com>
2020-04-27 12:25:49 +0300

    Второе задание

--- script.js
+++ script.js
@@ -2,21 +2,55 @@
 
 const money = +prompt("Ваш бюджет на месяц?");
 const time = prompt("Введите дату в формате YYYY-MM-DD");
-const expensesItemOne = prompt("Введите обязательную статью расходов в этом месяце");
-const expensesValueOne = +prompt("Во сколько обойдется?");
-const expensesItemTwo = prompt("Введите обязательную статью расходов в этом месяце");
-const expensesValueTwo = +prompt("Во сколько обойдется?");
+
 const appData = {
           budget: money,
           timeData: time,
-          expenses: {
-              [expensesItemOne]: [expensesValueOne],
-              [expensesItemTwo]: [expensesValueTwo],
-          },
+          expenses: {},
           optionalExpenses: {},
           income: [],
           savings: false,
       };
-const bugetPerDay = (money - expensesValueOne - expensesValueTwo)/30;
-      alert("Ваш бюджет на один день: " + bugetPerDay.toFixed(2));
+
+    enterValue: for (let i = 0; i < 2; i++) {
+        const a = prompt("Введите обязательную статью расходов в этом месяце");
+        const b = +prompt("Во сколько обойдется?");
+
+        if ((typeof(a) != null) && (typeof(b) != null) && a != '' && b != '' && a.length < 50) {
+            console.log("Done!");
+            appData.expenses[a] = b;
+        } else {
+            alert("Ведите корректное значение!");
+            continue enterValue;
+        }
+    }
+
+    // let i = 0;
+    // while (i < 2) {
+    //     const a = prompt("Введите обязательную статью расходов в этом месяце");
+    //     const b = +prompt("Во сколько обойдется?");
+    //     i++;
+    //     if ((typeof(a) != null) && (typeof(b) != null) && a != '' && b != '' && a.length < 50) {
+    //         console.log("Done!");
+    //         appData.expenses[a] = b;
+    //     } else {
+    //         alert("Ведите корректное значение!");
+    //     }
+    // }
+
+    // do {
+    //     const a = prompt("Введите обязательную статью расходов в этом месяце");
+    //     const b = +prompt("Во сколько обойдется?");
+    //     i++;
+    //     if ((typeof(a) != null) && (typeof(b) != null) && a != '' && b != '' && a.length < 50) {
+    //         console.log("Done!");
+    //         appData.expenses[a] = b;
+    //     } else {
+    //         alert("Ведите корректное значение!");
+    //     }
+    // } while (i < 2);
+
+
+    appData.budgetPerDay = appData.budget / 30;
+      alert("Ваш бюджет на один день: " + appData.budgetPerDay.toFixed(2));
 
