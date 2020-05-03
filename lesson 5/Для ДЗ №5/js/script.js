1e5621e20c284f7368dac9eebe92c6855e6ffc76
coffeemaniak <derrykain@gmail.com>
2020-05-03 17:26:27 +0300

    Пятый урок

---
+++ lesson 5/Для ДЗ №5/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let menuItem = document.getElementsByClassName("menu-item"),
+    menu = document.getElementsByClassName("menu")[0],
+    menuNewItem = document.createElement("li"),
+    title = document.getElementById("title"),
+    adv = document.getElementsByClassName("adv")[0],
+    body = document.getElementsByTagName("body")[0],
+    question = document.getElementById("prompt");
+
+menu.insertBefore(menuItem[2], menuItem[1]);
+menuNewItem.classList.add("menu-item");
+menuNewItem.textContent = "Пятый элемент";
+menu.appendChild(menuNewItem);
+
+title.textContent = "Мы продаем только подлинную технику Apple";
+
+adv.remove();
+
+body.style.backgroundImage = "url(img/apple_true.jpg)";
+
+let answer = prompt('Ваше отношение к технике Apple?');
+question.textContent = answer;
