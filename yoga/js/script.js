b654d2da08663f0bad2baa0672323b6bbc6adadf
coffeemaniak <derrykain@gmail.com>
2020-05-08 21:07:45 +0300

    Timer (and it's work!)

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
+    //tabs
 
     "use strict";
 
@@ -34,4 +35,54 @@ window.addEventListener("DOMContentLoaded", function () {
             }
         }
     });
+
+    //timer
+
+    let deadline = "2020-05-12"; //date of finish covid-carantin
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            "total": t,
+            "seconds": seconds,
+            "minutes": minutes,
+            "hours": hours,
+        };
+    }
+
+    function addZero(num) {
+        if (num <= 9) {
+            return "0" + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector(".hours"),
+            minutes = timer.querySelector(".minutes"),
+            seconds = timer.querySelector(".seconds"),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }
+        }
+    }
+
+    setClock("timer", deadline);
 });
