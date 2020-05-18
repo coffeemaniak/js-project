afef40c391418a2cc96361a8663aa12be2371411
coffeemaniak <derrykain@gmail.com>
2020-05-18 21:38:33 +0300

    movie project for new block of learning js

---
+++ movie-project/script.js
@@ -0,0 +1,16 @@
+let numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели?"),
+    personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false,
+    };
+
+let a = prompt("Один из просмотренных фильмов?"),
+    b = prompt("На сколько оцените его?"),
+    c = prompt("Один из просмотренных фильмов?"),
+    d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
