15901a2dc84ee8845676f1bca40a45d891278251
coffeemaniak <derrykain@gmail.com>
2020-06-10 18:41:57 +0300

    default and Rest

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -146,19 +146,27 @@ window.addEventListener("DOMContentLoaded", () => {
     //Card 
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
         }
 
         render() {
             const elem = document.createElement("div");
+
+            if (this.classes.length === 0) {
+                this.elem = "menu__item";
+                elem.classList.add(this.elem);
+            } else {
+                this.classes.forEach(className => elem.classList.add("menu__item"));
+            }
+
             elem.innerHTML = `
-                <div class="menu__item">
                         <img src=${this.src} alt=${this.alt}>
                         <h3 class="menu__item-subtitle">${this.title}</h3>
                         <div class="menu__item-descr">${this.descr}</div>
@@ -167,7 +175,6 @@ window.addEventListener("DOMContentLoaded", () => {
                             <div class="menu__item-cost">Цена:</div>
                             <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                         </div>
-                    </div>
                 `;
             this.parent.append(elem);
         }
