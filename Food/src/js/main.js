7d44ef84fed80b9927d282e580945a9670dda649
coffeemaniak <derrykain@gmail.com>
2020-06-21 20:36:02 +0300

    DB and slider

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -231,7 +231,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
             const formData = new FormData(form);
 
-            fetch("servet.php", {
+            fetch("server.php", {
                 method: "POST",
                 body: formData
             })
@@ -270,4 +270,54 @@ window.addEventListener("DOMContentLoaded", () => {
             closeModal();
         }, 5000);
     }
+
+    //slider
+
+    const slides = document.querySelectorAll(".offer__slide"),
+          prev = document.querySelector(".offer__slider-prev"),
+          next = document.querySelector(".offer__slider-next"),
+          current = document.querySelector("#current"),
+          total = document.querySelector("#total");
+    let slideIndex = 1;
+
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = "none");
+        slides[slideIndex - 1].style.display = "block";
+        current.textContent = slideIndex;
+
+        // if (slides.length < 10) {
+        //     current.textContent = `0${slideIndex}`;
+        // } else {
+        //     current.textContent = slideIndex;
+        // }
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener("click", () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener("click", () => {
+        plusSlides(1);
+    });
 });
