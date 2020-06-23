1212c59b10f570964bda1949a41113dac4f9f1cd
coffeemaniak <derrykain@gmail.com>
2020-06-23 20:25:02 +0300

    regExp

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -348,11 +348,15 @@ for (let i = 0; i < slides.length; i++) {
     dots.push(dot);
 }
 
+function deleteDigits (str) {
+    return +str.replace(/\D/g, "");
+}
+
 next.addEventListener("click", () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == deleteDigits(width) * (slides.length - 1)) {
         offset = 0;
     } else {
-        offset += +width.slice(0, width.length - 2);
+        offset += deleteDigits(width);
     }
 
     slidefields.style.transform = `translateX(-${offset}px)`;
@@ -375,9 +379,9 @@ next.addEventListener("click", () => {
 
 prev.addEventListener("click", () => {
     if (offset == 0) {
-        offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        offset = deleteDigits(width) * (slides.length - 1);
     } else {
-        offset -= +width.slice(0, width.length - 2);
+        offset -= deleteDigits(width);
     }
 
     slidefields.style.transform = `translateX(-${offset}px)`;
@@ -403,7 +407,7 @@ dots.forEach(dot => {
         const slideTo = e.target.getAttribute("data-slide-to");
 
         slideIndex = slideTo;
-        offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+        offset = deleteDigits(width) * (slideTo - 1);
 
         slidefields.style.transform = `translateX(-${offset}px)`;
 
@@ -416,7 +420,7 @@ dots.forEach(dot => {
         dots.forEach(dot => dot.style.opacity = ".5");
         dots[slideIndex - 1].style.opacity = 1;
     });
-})
+});
 
 // showSlides(slideIndex);
 
