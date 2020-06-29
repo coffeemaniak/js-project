89c66e9561a54a3c8a087644e201294760464dd7
coffeemaniak <derrykain@gmail.com>
2020-06-29 19:11:07 +0300

    modules - CommonJS

---
+++ Food/src/js/modules/forms.js
@@ -0,0 +1,70 @@
+function forms() {
+    //Forms
+
+    const forms = document.querySelectorAll("form");
+    const messages = {
+        loading: "img/form/original.svg",
+        success: "Спасибо! скоро мы с вами свяжемся",
+        failure: "Что-то пошло не так!"
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement("img");
+            statusMessage.src = messages.loading;
+            statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto;
+            `;
+            form.insertAdjacentElement("afterend", statusMessage);
+
+            const formData = new FormData(form);
+
+            fetch("server.php", {
+                    method: "POST",
+                    body: formData
+                })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(messages.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                    showThanksModal(messages.failure);
+                }).finally(() => {
+                    form.reset();
+                });
+        });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector(".modal__dialog");
+
+        prevModalDialog.classList.add("hide");
+        openModal();
+
+        const thanksModal = document.createElement("div");
+        thanksModal.classList.add("modal__dialog");
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div data-close class="modal__close">&times;</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        document.querySelector(".modal").append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add("show");
+            prevModalDialog.classList.remove("hide");
+            closeModal();
+        }, 5000);
+    }
+}
+
+module.exports = forms;
