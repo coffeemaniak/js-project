1060e2a9609a2bec04a9f31a4b2eb01e8ee4d250
coffeemaniak <derrykain@gmail.com>
2020-07-10 18:09:54 +0300

    tabs

--- Window/src/js/modules/modals.js
+++ Window/src/js/modules/modals.js
@@ -28,8 +28,16 @@ const modals = () => {
         });
     }
 
+    function showModalByTime(selector, time) {
+        setTimeout(function () {
+            document.querySelector(selector).style.display = "block";
+            document.body.style.overflow = "hidden";
+        }, time);
+    }
+
     bindModals(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
     bindModals(".phone_link", ".popup", ".popup .popup_close");
+    showModalByTime(".popup", 60000);
 };
 
 export default modals;
