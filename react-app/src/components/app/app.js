b5bb9b661d81c5a70495515e23d580d54d4875f8
coffeemaniak <derrykain@gmail.com>
2020-07-19 22:41:10 +0300

    start the structure and components

---
+++ react-app/src/components/app/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from "../header";
+import SearchPanel from '../search-panel';
+
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <SearchPanel/>
+        </div>
+    )
+}
+
+export default App;
---
+++ react-app/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Daria Yakovleva</h1>
+            <h2>5 записей, из них понравилось 1</h2>
+        </div>
+    )
+}
+
+export default Header;
