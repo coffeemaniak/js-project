9031e2cee7867df2399f630a9bc8e3073be64eb5
coffeemaniak <derrykain@gmail.com>
2020-07-20 23:00:28 +0300

    finish structure

---
+++ react-app/src/components/post-list-item/post-list-item.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const PostListItem = () => {
+    return (
+        <li className="app-list-item d-flex justify-content-between">
+            <span className="app-list-item-label">
+                First Hello World on React!!
+            </span>
+            <div className="d-flex justify-content-center align-items-center">
+                <button type="button"  className="btn-star btn-sm">
+                    <li className="fa fa-star"></li>
+                </button>
+                <button type="button" className="btn-trash btn-sm">
+                    <li className="fa fa-trash-o"></li>
+                </button>
+                <li className="fa fa-heart"></li>
+            </div>
+        </li>
+    )
+}
+
+export default PostListItem;
