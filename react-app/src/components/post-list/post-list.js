9ca10a0cac65da9a35384f7c1cefac0f00f8f5c9
coffeemaniak <derrykain@gmail.com>
2020-07-24 20:49:05 +0300

    some fix

--- react-app/src/components/post-list/post-list.js
+++ react-app/src/components/post-list/post-list.js
@@ -2,17 +2,31 @@ import React from 'react';
 import PostListItem from "../post-list-item";
 import "./post-list.css";
 
+
 const PostList = ({posts}) => {
+
     const elements = posts.map((item) => {
-        return (
+
+        if(typeof item === "object" && isEmpty(item)) {
+
+            return (
             <li key={item.id} className="list-group-item">
                 <PostListItem 
                 label={item.label}
                 important={item.important}
                 />
             </li>
-        )
+            )
+            
+        }
     })
+    
+    function isEmpty(obj) {
+        for (let key in obj) {
+            return true;
+        }
+        return false;
+    }
 
     return (
         <ul className="app-list list-group">
